c4f16ade663177f24f3bb8c5e4a460d74fa634e0
Uros <uros.tripunovic@omegapoint.se>
2025-03-13 07:23:26 +0100

    Update ts-tools

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -4,7 +4,20 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  config = function()
-    require('typescript-tools').setup {}
-  end,
+  ft = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact' },
+  opts = {
+    on_attach = function(client, bufnr)
+      -- Disable formatting (use prettier instead, see `conform.lua`)
+      -- client.server_capabilities.documentFormattingProvider = false
+      -- client.server_capabilities.documentRangeFormattingProvider = false
+
+      local map = vim.keymap.set
+
+      map('n', '<leader>lo', '<cmd>TSToolsOrganizeImports<CR>', { desc = 'LSP Organize imports' })
+      map('n', '<leader>li', '<cmd>TSToolsAddMissingImports<CR>', { desc = 'LSP add missing imports' })
+      map('n', '<leader>lf', '<cmd>TSToolsFixAll<CR>', { desc = 'LSP fix all errors' })
+      map('n', '<leader>lu', '<cmd>TSToolsRemoveUnused<CR>', { desc = 'LSP remove unused' })
+      map('n', '<leader>lr', '<cmd>TSToolsRenameFile<CR>', { desc = 'LSP rename file' })
+    end,
+  },
 }
