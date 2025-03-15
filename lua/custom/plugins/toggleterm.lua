d08e520f4c406c0f7b4e43363b0d3c3e98a85237
Uros <uros.tripunovic@omegapoint.se>
2025-03-15 09:57:34 +0100

    Add movements to toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,6 +5,7 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     local toggleterm = require 'toggleterm'
+    local map = vim.keymap.set
 
     toggleterm.setup {
       open_mapping = '<M-\\>', -- '<c-\\>', -- fuck you windows
@@ -22,5 +23,21 @@ return {
       },
       size = vim.o.columns * 0.25,
     }
+    local function set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      map('t', '<esc>', [[<C-\><C-n>]], { buffer = true })
+      map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      map('t', '<C-w>', [[<C-\><C-n><C-w>]], { buffer = true })
+    end
+
+    vim.api.nvim_create_autocmd('TermOpen', {
+      pattern = 'term://*toggleterm#*',
+      callback = function()
+        set_terminal_keymaps()
+      end,
+    })
   end,
 }
